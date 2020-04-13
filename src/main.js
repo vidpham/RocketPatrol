94f69b1747bedf3a2808713e083d212f1c821dd3
Vivian Pham <vidpham@ucsc.edu>
2020-04-13 00:48:36 -0700

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 let config = {
-   type: Phaser.CANVAS,
+   type: Phaser.AUTO,
    width: 640,
-   height: 480
+   height: 480,
+   scene: [ Menu, Play ]
 }
+
 let game = new Phaser.Game(config);
+
+// define game settings
+game.settings = {
+   spaceshipSpeed: 3,
+   gameTimer: 60000    
+}
+
+// reserve keyboard vars
+let keyF, keyLEFT, keyRIGHT;
+
